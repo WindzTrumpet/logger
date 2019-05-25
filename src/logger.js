99c297a907968ddb9ccd3946aca0df921805f62b
WindzTrumpet <chinapat.k@facgure.com>
2019-05-25 12:54:40 +0700

    Create logger and add gitignore

---
+++ src/logger.js
@@ -0,0 +1,33 @@
+'use strict';
+const moment = require('moment-timezone')
+const fs = require('fs');
+const logDir = 'logs';
+
+function createLog(type, message) {
+	var now = new Date();
+	var dateString = moment(now).format('YYYY-MM-DD');
+	var pathLog = logDir + '/'+ dateString
+	var logDateTime = moment(now).format('YYYY-MM-DD HH:mm:ss');
+	
+	if (!fs.existsSync(pathLog)) {
+		fs.mkdirSync(pathLog);
+	}
+
+	var path = pathLog + `/${type}-${dateString}.log`
+
+	fs.appendFile(path, `${logDateTime} ${type} ${message}\n`, (err) => {
+		if(err) return console.log('Logger error!');
+	})
+}
+
+module.exports.info = function(message) {
+	createLog('info', message)
+}
+
+module.exports.error = function(message) {
+	createLog('error', message)
+}
+
+module.exports.verbose = function(message) {
+	createLog('verbose', message)
+}
